8166b6d94997c762944b5f958764a7fd7f3a94e0
A-Awad1 <eng.a.awad1@gmail.com>
2024-01-04 11:12:10 +0200

    updates in destinations view

--- src/views/DestinationsView.vue
+++ src/views/DestinationsView.vue
@@ -4,25 +4,28 @@
       <PageNumberSpan />
       <h2>pick your destination</h2>
     </header>
-    <ul>
-      <li
-        v-for="dest in allDest"
-        :key="dest"
-        v-text="dest"
-        @click="currentDestName = dest"
-      ></li>
-    </ul>
-    <h3 v-text="currentDest.name"></h3>
-    <p v-text="currentDest.description"></p>
-    <hr />
-    <div>
-      <span>avg. distance</span>
-      <span v-text="currentDest.distance"></span>
-    </div>
-    <div>
-      <span>est. travel time</span>
-      <span v-text="currentDest.travel"></span>
-    </div>
+    <main>
+      <ul>
+        <li
+          v-for="dest in allDest"
+          :key="dest"
+          v-text="dest"
+          @click="currentDestName = dest"
+        ></li>
+      </ul>
+      <h3 v-text="currentDest.name"></h3>
+      <p v-text="currentDest.description"></p>
+      <hr />
+      <div>
+        <span>avg. distance</span>
+        <span v-text="currentDest.distance"></span>
+      </div>
+      <div>
+        <span>est. travel time</span>
+        <span v-text="currentDest.travel"></span>
+      </div>
+      <img :src="currentImg" />
+    </main>
   </section>
 </template>
 
@@ -47,10 +50,57 @@ export default {
         (e) => e.name === this.currentDestName
       )[0];
     },
+    currentImg: function () {
+      return require(`../assets/destinations/image-${this.currentDestName.toLowerCase()}.png`);
+    },
   },
   components: {
     PageNumberSpan,
   },
 };
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+section.destinations {
+  display: flex;
+  flex-direction: column;
+  text-transform: uppercase;
+  > {
+    header {
+      @extend %center-flex;
+      gap: 18px;
+      h2 {
+        font: {
+          size: 16px;
+          weight: normal;
+        }
+        letter-spacing: 2.7px;
+        line-height: normal;
+      }
+    }
+    main {
+      > {
+        ul {
+          li {
+          }
+        }
+        h3 {
+        }
+        p {
+          text-transform: none;
+        }
+        hr {
+        }
+        div {
+          span:first-child {
+          }
+          span:last-child {
+          }
+        }
+        img {
+          width: 170px;
+        }
+      }
+    }
+  }
+}
+</style>
