54723723ba113e7f1dae9cea6f655b4ee82d5876
A-Awad1 <eng.a.awad1@gmail.com>
2024-01-04 07:31:31 +0200

    update destinations view logic

--- src/views/DestinationsView.vue
+++ src/views/DestinationsView.vue
@@ -4,7 +4,15 @@
       <PageNumberSpan />
       <h2>pick your destination</h2>
     </header>
-    <ul></ul>
+    <ul>
+      <li
+        v-for="dest in allDest"
+        :key="dest"
+        v-text="dest"
+        @click="currentDest = dest"
+      ></li>
+    </ul>
+    {{ currentDestData }}
   </section>
 </template>
 
@@ -14,6 +22,21 @@ export default {
   name: "DestinationsView",
   inject: {
     siteData: "siteData",
+  },
+  data() {
+    return {
+      currentDest: this.siteData.destinations.map((e) => e.name)[0],
+    };
+  },
+  computed: {
+    allDest: function () {
+      return this.siteData.destinations.map((e) => e.name);
+    },
+    currentDestData: function () {
+      return this.siteData.destinations.filter(
+        (e) => e.name === this.currentDest
+      );
+    },
   },
   components: {
     PageNumberSpan,
